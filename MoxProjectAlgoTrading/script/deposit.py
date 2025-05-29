72e37ed99ea05d4f97f1c46d0b49f6507f1974a2
Pacifist <knyazevmatvey4@gmail.com>
2025-05-29 18:56:13 +0500

    Deleted non used files

--- MoxProjectAlgoTrading/script/deposit.py
+++
@@ -1,8 +0,0 @@
-from moccasin.config import get_active_network
-import boa 
-active_netwrork = get_active_network()
-usdc = active_netwrork.manifest_named("usdc")
-weth = active_netwrork.manifest_named("weth")
-print("ETH:", boa.env.get_balance(boa.env.eoa))
-print("WETH:", weth.balanceOf(boa.env.eoa))
-print("USDC:", usdc.balanceOf(boa.env.eoa))
